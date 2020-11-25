2ea090c956ec201e7f7eb6893cd82005703ad13b
EdwardKV <edwardkv@gmail.com>
2020-11-25 14:04:06 +0400

    fix typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @questions = @user.questions.order(create_at: :desc)
+    @questions = @user.questions.order(created_at: :desc)
 
     @new_question = @user.questions.build
 
